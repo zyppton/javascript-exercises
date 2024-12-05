f8329449637d06718503ba7fe9f99394c842b50f
zyppton <jasonconlow@gmail.com>
2024-12-05 10:57:18 -0800

    finished 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word, times) {
+    const theWord = word;
+    let result = "";
+    if (times > -1) {
+        for (let i = times; i > 0; i--) {
+            result = result + theWord;
+        }
+        return result;
+    }else{
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
