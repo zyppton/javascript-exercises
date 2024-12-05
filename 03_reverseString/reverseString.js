3bf5f4d47a74112d48ec43b9de8229f386379d53
zyppton <jasonconlow@gmail.com>
2024-12-05 11:16:55 -0800

    finished 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(input) {
+    let phrase = input;
+    let phraseLength = phrase.length;
+    let reversePhrase = "";
 
+    for (let i = phraseLength; i >= 0; i--) {
+        reversePhrase = reversePhrase + phrase.charAt(i);
+    }
+
+    return reversePhrase;
 };
 
 // Do not edit below this line
