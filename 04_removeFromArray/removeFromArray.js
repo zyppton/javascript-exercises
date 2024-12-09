c3a2998749f8024f4bbe5ba8b9ef283b36b4dd32
zyppton <jasonconlow@gmail.com>
2024-12-08 21:54:29 -0800

    finished 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+    let list = array;
+    let listLength = list.length;
+    for (const arg of values) {
+        let remove = arg;
+
+
+        for (let i = 0; i < listLength; i++){
+            if (remove === list[i]) {
+                list.splice(i, 1);
+                i--;
+            }
+        }
+
+    }
+    return list;
 };
 
 // Do not edit below this line
