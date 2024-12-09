7e2b8852258ad53d4531deea73d4c17bb23c7064
zyppton <jasonconlow@gmail.com>
2024-12-09 15:00:33 -0800

    finished 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    let sum = 0;
+
+    if (numOne >= 0 && numTwo >=0 && Number.isInteger(numOne) && Number.isInteger(numTwo)) {
+        if (numOne < numTwo) {
+            for (let i = numOne; i <= numTwo; i++) {
+                sum = sum + i;
+            }
+        } else {
+            for (let i = numOne; i >= numTwo; i--) {
+                sum = sum + i;
+            }
+        }   
+        return sum;
+    }else {
+        return "ERROR";
+    }
+
 
 };
 
