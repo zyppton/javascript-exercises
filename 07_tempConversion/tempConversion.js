18cb9131b3f344e15a947ad18a8000eb5b38144d
zyppton <jasonconlow@gmail.com>
2024-12-09 15:27:24 -0800

    finished 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let celsius = (temp - 32) * (5/9);
+  let rounded = Math.round(celsius * 10) / 10;
+  return rounded;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let fahrenheit = (temp * (9/5)) + 32;
+  let rounded = Math.round(fahrenheit * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
