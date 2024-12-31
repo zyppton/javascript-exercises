7f2397fe5f076bc1f5a22eddca569598643d770e
zyppton <jasonconlow@gmail.com>
2024-12-30 23:10:42 -0800

    09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    const wrd = word;
+    let newWord = ""
+    let reverse = "";
 
+    for (i = 0; i <= (wrd.length - 1); i++) {
+        if (/^[a-zA-Z]$/.test(wrd[i]) || !isNaN(wrd[i]) && wrd[i] != " ") {
+            newWord += wrd[i].toLowerCase();
+        }
+    }
+
+    for (i = (wrd.length - 1); i >= 0; i--) {
+        if (/^[a-zA-Z]$/.test(wrd[i]) || !isNaN(wrd[i]) && wrd[i] != " ") {
+            reverse += wrd[i].toLowerCase();
+        }
+    }
+    if (newWord === reverse) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+// console.log(palindromes("thing"));
 
 // Do not edit below this line
 module.exports = palindromes;
