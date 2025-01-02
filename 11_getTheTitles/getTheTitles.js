b001a6cd6e2c35460353858646b03dc1dfb3b2d2
zyppton <jasonconlow@gmail.com>
2025-01-02 08:55:36 -0800

    11 done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+    const titles = array.map(book => book.title)
 
+    return titles;
 };
 
 // Do not edit below this line
